98e63c39e2fcf9516bb71bca79e7f4e400f9fe2f
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-10-28 17:48:16 -0400

    add media queries

--- js/youtube.js
+++ js/youtube.js
@@ -1,47 +1,45 @@
-window.onload = e =>
-{
-    /************************************************************************************************************************** */
-    const sideMenu = document.querySelector(".side");
-    const sideMenuItems = document.querySelectorAll(".side-item-field");
-    
-    let itema;
-    
-    sideMenu.addEventListener("click", function(e){
-       const target = e.target;
-       sideMenuItems.forEach((item) => 
-        {
-            if(item.classList.contains("active-side-item-field"))
-            {
-               itema = item;
-            }
-        })
-        if(target.classList.contains("side-item-field"))
-        {
-            target.classList.toggle("active-side-item-field");
-            itema.classList.toggle("active-side-item-field");
-        }
+window.onload = (e) => {
+  /************************************************************************************************************************** */
+  const sideMenu = document.querySelector(".side");
+  const sideMenuItems = document.querySelectorAll(".side-item-field");
 
-    })
+  let itema;
 
-    /**************************************************************************************************************************** */
-    const centerMenu = document.querySelector(".theme-content-buttons-container"); 
-    const centerMenuItems = document.querySelectorAll(".theme-content-button"); 
-    let itemb;
+  sideMenu.addEventListener("click", function (e) {
+    const target = e.target;
+    sideMenuItems.forEach((item) => {
+      if (item.classList.contains("active-side-item-field")) {
+        itema = item;
+      }
+    });
+    if (target.classList.contains("side-item-field")) {
+      target.classList.toggle("active-side-item-field");
+      itema.classList.toggle("active-side-item-field");
+    }
+  });
 
-    centerMenu.addEventListener("click", function(e){
-       const target = e.target;
-       centerMenuItems.forEach((item) => 
-        {
-            if(item.classList.contains("active"))
-            {
-               itemb = item;
-            }
-        })
-       target.classList.toggle("active");
-       itemb.classList.toggle("active");
-    })
+  /**************************************************************************************************************************** */
+  const centerMenu = document.querySelector(".theme-content-buttons-container");
+  const centerMenuItems = document.querySelectorAll(".theme-content-button");
+  let itemb;
 
-    /************************************************************************************************************************** */
-   
+  centerMenu.addEventListener("click", function (e) {
+    const target = e.target;
+    centerMenuItems.forEach((item) => {
+      if (item.classList.contains("active")) {
+        itemb = item;
+      }
+    });
+    target.classList.toggle("active");
+    itemb.classList.toggle("active");
+  });
 
+  /************************************************************************************************************************** */
+
+  const myWebsiteButton = document.querySelector(".top-login");
+  myWebsiteButton.addEventListener("click", goToMyWebsite);
+};
+
+function goToMyWebsite() {
+  window.open("https://www.marcelo-brasil.com");
 }
